08b46b39390e8e681de6894686652bdd30f8c9e3
PunPK <phurutsakorn.kps@gmail.com>
2024-12-17 23:23:33 +0700

    EditButton Succeed : Fix Function Edit add Add popup EEdit

--- src/FinanceScreen.js
+++ src/FinanceScreen.js
@@ -1,11 +1,12 @@
-import "./App.css";
-import TransactionList from "./components/TransactionList";
-import { useState, useEffect } from "react";
-import dayjs from "dayjs";
-import { Divider } from "antd";
+import React, { useState, useEffect } from "react";
+import { Spin, Typography, Divider, Button } from "antd";
 import AddItem from "./components/AddItem";
-import { Spin, Typography } from "antd";
+import TransactionList from "./components/TransactionList";
+import Modal from "./components/EditItem";
 import axios from "axios";
+import dayjs from "dayjs";
+// import Nav from "./components/nav";
+// import { useNavigate } from "react-router-dom";
 
 const URL_TXACTIONS = "/api/txactions";
 
@@ -13,32 +14,55 @@ function FinanceScreen() {
   const [summaryAmount, setSummaryAmount] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [transactionData, setTransactionData] = useState([]);
-  const [EditData, setEditData] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [editingRecord, setEditingRecord] = useState(null);
+  // const navigate = useNavigate();
+
+  const openModal = (record) => {
+    setEditingRecord(record);
+    setIsModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+    setEditingRecord(null);
+  };
 
   const fetchItems = async () => {
     try {
       setIsLoading(true);
       const response = await axios.get(URL_TXACTIONS);
-      setTransactionData(
-        response.data.data.map((row) => ({
-          id: row.id,
-          key: row.id,
-          ...row.attributes,
-        }))
-      );
+      const data = response.data.data.map((row) => ({
+        id: row.id,
+        key: row.id,
+        ...row.attributes,
+      }));
+      setTransactionData(data);
     } catch (err) {
       console.log(err);
     } finally {
       setIsLoading(false);
     }
   };
+  const handleNoteChanged = (id, note) => {
+    setTransactionData(
+      transactionData.map((transaction) => {
+        transaction.note = transaction.id === id ? note : transaction.note;
+        return transaction;
+      })
+    );
+  };
 
-  const handleAddItem = async (item) => {
+  const handleRowEdited = async (item) => {
     try {
       setIsLoading(true);
-      const params = { ...item, action_datetime: dayjs() };
-      const response = await axios.post(URL_TXACTIONS, { data: params });
+      // const params = { ...item, action_datetime: dayjs() };
+      const response = await axios.put(`${URL_TXACTIONS}/${item.id}`, {
+        data: item,
+      });
+      fetchItems();
       const { id, attributes } = response.data.data;
+
       setTransactionData([
         ...transactionData,
         { id: id, key: id, ...attributes },
@@ -50,19 +74,10 @@ function FinanceScreen() {
     }
   };
 
-  const handleNoteChanged = (id, note) => {
-    setTransactionData(
-      transactionData.map((transaction) => {
-        transaction.note = transaction.id === id ? note : transaction.note;
-        return transaction;
-      })
-    );
-  };
-
-  const handleRowDeleted = async (id) => {
+  const handleRowDeleted = async (itemId) => {
     try {
       setIsLoading(true);
-      await axios.delete(`${URL_TXACTIONS}/${id}`);
+      await axios.delete(`${URL_TXACTIONS}/${itemId}`);
       fetchItems();
     } catch (err) {
       console.log(err);
@@ -70,18 +85,9 @@ function FinanceScreen() {
       setIsLoading(false);
     }
   };
-
-  const updateItem = async (id, item) => {
-    try {
-      setIsLoading(true);
-      await axios.put(`${URL_TXACTIONS}/${item.id}`, { data: item });
-      setEditData([...transactionData, { id: id, key: id }]);
-    } catch (err) {
-      console.log(err);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  // const handleEditClick = () => {
+  //   navigate("/Home");
+  // };
 
   useEffect(() => {
     fetchItems();
@@ -101,22 +107,30 @@ function FinanceScreen() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header></header>
+      <body className="App-finance-body">
         <Spin spinning={isLoading}>
           <Typography.Title>
             จำนวนเงินปัจจุบัน {summaryAmount} บาท
           </Typography.Title>
 
-          <AddItem onItemAdded={handleAddItem} />
           <Divider>บันทึก รายรับ - รายจ่าย</Divider>
           <TransactionList
             data={transactionData}
+            onRowEdited={openModal}
             onNoteChanged={handleNoteChanged}
             onRowDeleted={handleRowDeleted}
-            updateItem={updateItem}
           />
+          {isModalVisible && (
+            <Modal
+              defaultValue={editingRecord}
+              closeModal={closeModal}
+              onSubmit={handleRowEdited}
+            />
+          )}
+          {/* <Button onClick={handleEditClick}>Go to Home</Button> */}
         </Spin>
-      </header>
+      </body>
     </div>
   );
 }
--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -1,42 +1,84 @@
-import { Button, Form, Select, Input, InputNumber } from "antd";
+import React, { useEffect } from "react";
+import { Modal, Form, Input, InputNumber, Button, Select } from "antd";
 
-export default function EditItem(props) {
+const Edit = ({ defaultValue, closeModal, onSubmit }) => {
+  const [form] = Form.useForm();
+
   useEffect(() => {
-    Form.setFieldsValue(props.item);
-  }, []);
+    if (defaultValue) {
+      form.setFieldsValue({
+        id: defaultValue.id,
+        note: defaultValue.note,
+        amount: defaultValue.amount,
+      });
+    }
+  }, [defaultValue, form]);
+
+  const handleSubmit = () => {
+    form.validateFields().then((values) => {
+      const updatedRecord = {
+        ...defaultValue,
+        ...values,
+      };
+      console.log("Updated Record:", updatedRecord);
+      onSubmit(updatedRecord);
+      closeModal();
+    });
+  };
 
   return (
-    <Form layout="inline" onFinish={props.onItemAdded}>
-      <Form.Item name="type" label="ชนิด" rules={[{ required: true }]}>
-        <Select
-          allowClear
-          style={{ width: "100px" }}
-          options={[
-            {
-              value: "income",
-              label: "รายรับ",
-            },
-            {
-              value: "expense",
-              label: "รายจ่าย",
-            },
-          ]}
-        />
-      </Form.Item>
+    <Modal
+      title="Edit transaction"
+      open={true}
+      onCancel={closeModal}
+      footer={[
+        <Button key="cancel" onClick={closeModal}>
+          Cancel
+        </Button>,
+        <Button key="submit" type="primary" onClick={handleSubmit}>
+          Edit
+        </Button>,
+      ]}
+    >
+      <Form form={form} layout="vertical">
+        <Form.Item name="id" label="ID" hidden>
+          <Input disabled />
+        </Form.Item>
 
-      <Form.Item name="amount" label="จำนวนเงิน" rules={[{ required: true }]}>
-        <InputNumber placeholder="จำนวนเงิน" />
-      </Form.Item>
+        <Form.Item
+          name="type"
+          label="ชนิด"
+          rules={[{ required: true, message: "กรุณาเลือกชนิด!" }]}
+          layout="horizontal"
+        >
+          <Select
+            allowClear
+            style={{ width: "100px" }}
+            options={[
+              { value: "income", label: "รายรับ" },
+              { value: "expense", label: "รายจ่าย" },
+            ]}
+          />
+        </Form.Item>
 
-      <Form.Item name="note" label="หมายเหตุ" rules={[{ required: true }]}>
-        <Input placeholder="Note" />
-      </Form.Item>
+        <Form.Item
+          name="amount"
+          label="จำนวนเงิน"
+          rules={[{ required: true, message: "กรุณากรอกจำนวนเงิน!" }]}
+        >
+          <InputNumber placeholder="จํานวนเงิน" />
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Add
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          name="note"
+          label="หมายเหตุ"
+          rules={[{ required: true, message: "กรุณากรอกหมายเหตุ!" }]}
+        >
+          <Input.TextArea rows={1} />
+        </Form.Item>
+      </Form>
+    </Modal>
   );
-}
+};
+
+export default Edit;
